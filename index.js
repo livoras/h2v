3c944a942dd02dbb52728304c2767635a23e1569
livoras <livoras@163.com>
2015-12-09 01:07:23 +0800

    add framework

--- index.js
+++ index.js
@@ -7,7 +7,38 @@ function h2v (html) {
   root = (root.childNodes.length === 1)
     ? root.childNodes[0]
     : root
-  return el(root.tagName.toLowerCase())
+  return toVirtualDOM(root)
+}
+
+function toVirtualDOM (dom) {
+  var tagName = dom.tagName.toLowerCase()
+  var props = attrToObj(dom)
+  var children = []
+  for (var i = 0, len = dom.childNodes.length; i < len; i++) {
+    var node = dom.childNodes[i]
+    // TEXT node
+    if (node.type === 3) {
+      if (node.nodeValue) {
+        children.push(node.nodeValue)
+      } else {
+        children.push(node.textContent)
+      }
+    } else {
+      children.push(toVirtualDOM(node))
+    }
+  }
+  return el(tagName, props, children)
+}
+
+function attrToObj (dom) {
+  var attrs = dom.attributes
+  var props = {}
+  for (var i = 0, len = attrs.length; i < len; i++) {
+    var name = attrs[i].name
+    var value = attrs[i].value
+    props[name] = value
+  }
+  return props
 }
 
 module.exports = h2v
