59bc8958c0853611a3eb410b283ecfd2ee307316
livoras <livoras@163.com>
2015-12-09 20:06:39 +0800

    fix style

--- test/h2v.spec.js
+++ test/h2v.spec.js
@@ -23,20 +23,20 @@ describe('Make virtual-dom from html', function () {
   it('DOM\'s innerHTML generated from virtual-dom should be the same with original html string', function () {
     var breakREG = /\\r\\n/g
     var htmls = [
-        fs.readFileSync(__dirname + '/fixtures/test1.html', 'utf-8'),
-        fs.readFileSync(__dirname + '/fixtures/test2.html', 'utf-8'),
-        fs.readFileSync(__dirname + '/fixtures/test3.html', 'utf-8')
+      fs.readFileSync(__dirname + '/fixtures/test1.html', 'utf-8'),
+      fs.readFileSync(__dirname + '/fixtures/test2.html', 'utf-8'),
+      fs.readFileSync(__dirname + '/fixtures/test3.html', 'utf-8')
     ]
     for (var i = 0, len = htmls.length; i < len; i++) {
-        var htmlString = htmls[i]
-        var el = h2v(htmlString)
-        var dom = el.render();
-        var div = document.createElement('div')
-        div.appendChild(dom)
-        var domString = div.innerHTML.replace(breakREG, '\\n')
-        htmlString = htmlString.replace(/\r\n/g, '\n')
-        domString.length.should.be.equal(htmlString.length)
-        domString.should.be.equal(htmlString)
+      var htmlString = htmls[i]
+      var el = h2v(htmlString)
+      var dom = el.render()
+      var div = document.createElement('div')
+      div.appendChild(dom)
+      var domString = div.innerHTML.replace(breakREG, '\\n')
+      htmlString = htmlString.replace(/\r\n/g, '\n')
+      domString.length.should.be.equal(htmlString.length)
+      domString.should.be.equal(htmlString)
     }
   })
 })
